7072af838755665dcfa78ab8e9b749d2b50eb112
JackyMeslin <jackymeslin@gmail.com>
2022-04-19 01:56:51 +0200

    confi & coverage

---
+++ config/routes.rb
@@ -0,0 +1,31 @@
+LandingPages::Engine.routes.draw do
+  resources :page, constraints: AdminConstraint.new do
+    member do
+      get "export" => "page#export"
+    end
+    collection do
+      post "upload" => "page#upload"
+    end
+  end
+
+  resource :remote, constraints: AdminConstraint.new do
+    collection do
+      get "pages" => "remotes#import"
+      post "test" => "remotes#test"
+      get "commits-behind" => "remotes#commits_behind"
+    end
+  end
+
+  resource :global, constraints: AdminConstraint.new
+
+  post "contact" => "landing#contact"
+  post "subscription" => "landing#subscription"
+  get "topic-list" => "landing#topic_list"
+end
+
+Discourse::Application.routes.prepend do
+  mount ::LandingPages::Engine, at: "landing"
+  get "/admin/plugins/landing-pages" => "admin/plugins#index", constraints: AdminConstraint.new
+  get "/:path" => "landing_pages/landing#show", constraints: LandingPageConstraint.new
+  get "/:path/:param" => "landing_pages/landing#show", constraints: LandingPageConstraint.new
+end
